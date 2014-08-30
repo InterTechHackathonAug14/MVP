4e0c975c016bdca88f18429f3840c1f6ea941f97
jblok <jonathon@jblok.co.uk>
2014-08-30 19:29:17 +0100

    Waiting for the url to push data to

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,26 +1,31 @@
 
   function postLocation() {
-		console.log(currentPlace);
+		console.log('currentPlace', currentPlace);
 		var text = $('#services').val();
-		console.log(text);
-		addToWhiteList({
-		placeId: currentPlace.id,
-		lat: currentPlace.geometry.location.k,
-		lng: currentPlace.geometry.location.B,
-		services: text});
+		console.log('servicesText', text);
+		// addToWhitelist({
+		// 	placeId: currentPlace.place_id,
+		// 	lat: currentPlace.geometry.location.k,
+		// 	lng: currentPlace.geometry.location.B,
+		// 	services: text
+		// });
   }
 
 
 	function addToWhitelist(data){
 		console.log(data);
+		var url = "";
 
-		// $.ajax(url, {
-		// 	method: "POST",
-		// 	data: {
-		// 		//placeId,
-		// 		//latLng,
-		// 		//services
-		// 	},
-		//
-		// });
+		$.ajax(url, {
+			method: "POST",
+		 	data: data,
+			success: function(){
+				console.log('Added succesfully');
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				console.log('jqXHR', jqXHR);
+				console.log('textStatus', textStatus);
+				console.log('errorThrown', errorThrown);
+			}
+		});
 	}
