d2f6ec3867b11504da5bebac1af6695c8505b35e
Molibar <christer.brannstrom@gmail.com>
2014-08-30 19:15:12 +0100

    Fixed the long/lat/id thingelibob

--- admin/js/app.js
+++ admin/js/app.js
@@ -4,11 +4,12 @@
 	}
 
 	  function postLocation() {
+		console.log(currentPlace);
 		var text = $('#services').val();
 		console.log(text);
 		addToWhiteList({
-		placeId: 123,
-		lat: 123,
-		lng: 123,
+		placeId: currentPlace.id,
+		lat: currentPlace.geometry.location.k,
+		lng: currentPlace.geometry.location.B,
 		services: text});
 	  }
