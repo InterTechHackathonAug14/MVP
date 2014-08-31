f38c707d2292dddf5d5ece4106b4378e4ebc1433
Molibar <christer.brannstrom@gmail.com>
2014-08-31 01:27:47 +0100

    Old API obsoletified

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,10 +1,10 @@
-(function(){
+var app = (function(){
 
 	var geocoder;
 	var map;
 	var infowindow;
 	var currentPlace = null;
-	var frmTpl = '<form id="frmSubmit"><div id="infoWindow"><div>{0}: {1}</div><div>Services</div><div><input id="services" type="text"/></div><button type="button" onclick="postLocation($(\'frmSubmit\'));">Submit</button></div></form>';
+	var frmTpl = '<form id="frmSubmit"><div id="infoWindow"><div>{0}: {1}</div><div>Services</div><div><input id="services" type="text"/></div><button type="button" onclick="app.postLocation();">Submit</button></div></form>';
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -88,25 +88,31 @@
 		console.log('currentPlace', currentPlace);
 		var text = $('#services').val();
 		console.log('servicesText', text);
-		addToWhitelist("google_id=" + currentPlace.place_id);
-		//{
-			//placeId: currentPlace.place_id,
-			//lat: currentPlace.geometry.location.k,
-			//lng: currentPlace.geometry.location.B,
-			//services: text
-		//});
+		var data = {
+			placeId: currentPlace.place_id,
+			lat: currentPlace.geometry.location.k,
+			lng: currentPlace.geometry.location.B,
+			services: text
+		};
+		
+		var url = "http://178.62.26.98:2222/place/{0}".replace("{0}", data.placeId);
+		$.get( url, function( data ) {
+			updateWhitelist(data);
+		}).fail(function() {
+			addToWhitelist(data);
+		});
 	}
 
 
 	function addToWhitelist(data){
 		console.log(data);
-		var url = "http://178.62.26.98:2222/places";
-
+		//return;
+		var url = "http://178.62.26.98:2222/place/{0}".replace("{0}", data.placeId);
 		$.ajax(url, {
 			method: "POST",
-		 	data: data,
+		 	data: "tags=" + data.services,
 			success: function(){
-				console.log('Added succesfully');
+				console.log('Added');
 			},
 			error: function(jqXHR, textStatus, errorThrown){
 				console.log('jqXHR', jqXHR);
@@ -116,6 +122,66 @@
 		});
 	}
 
+	
+	
+	function updateWhitelist(data) {
+		console.log(data);
+		//return;
+		var url = "http://178.62.26.98:2222/place/{0}".replace("{0}", data.placeId);
+		$.ajax(url, {
+			method: "DELETE",
+			success: function(){
+				addToWhitelist(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown){
+				console.log('jqXHR', jqXHR);
+				console.log('textStatus', textStatus);
+				console.log('errorThrown', errorThrown);
+			}
+		});
+	}
+
+
+	//function xxx() {
+	//	var url = "http://178.62.26.98:2222/places";
+	//	//var xhr = createCORSRequest("GET", url);
+	//	//if (!xhr) {
+	//	//	throw new Error('CORS not supported');
+	//	//}
+	//	//console.log(xhr.responseText);
+	//	//return;
+	//	$.get( url, function( data ) {
+	//		console.log(data);
+	//	}).fail(function() {
+	//		alert( "error" );
+	//	});
+	//}
+
+	//function createCORSRequest(method, url) {
+	//	var xhr = new XMLHttpRequest();
+	//	if ("withCredentials" in xhr) {
+	//	
+	//		// Check if the XMLHttpRequest object has a "withCredentials" property.
+	//		// "withCredentials" only exists on XMLHTTPRequest2 objects.
+	//		xhr.open(method, url, true);
+	//	
+	//	} else if (typeof XDomainRequest != "undefined") {
+	//	
+	//		// Otherwise, check if XDomainRequest.
+	//		// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+	//		xhr = new XDomainRequest();
+	//		xhr.open(method, url);
+	//	
+	//	} else {
+	//	
+	//		// Otherwise, CORS is not supported by the browser.
+	//		xhr = null;
+	//	
+	//	}
+	//	return xhr;
+	//}
+
+	
 
 	//function getFromWhitelist(id){
 	//	console.log(id);
@@ -127,4 +193,7 @@
 	//		alert( "error" );
 	//	});
 	//}
+	return {
+		postLocation: postLocation
+	};
 }());
